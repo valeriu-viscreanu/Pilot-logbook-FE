031908209578c5fffca8beda5db07436e43a3b36
Valeriu <Valeriu@LAPTOP-EJ86HI8K>
2020-04-29 17:20:35 +0300

    table borders and EASA .050 style selection

--- src/app/flight-hours/list/list.component.ts
+++ src/app/flight-hours/list/list.component.ts
@@ -12,7 +12,12 @@ export class ListComponent implements OnInit {
 
    Flights: Flight[] =[
      new Flight ( "DV20",  new Date("2016-01-17"), new Date("2016-01-17"), "Jane Doe"),
-     new Flight ( "DA40", new Date("2016-01-18"), new Date("2016-01-19"), "John Doe")
+     new Flight ( "DA40", new Date("2016-01-18"), new Date("2016-01-18"), "John Doe"),
+     new Flight ( "DA40", new Date("2016-01-19"), new Date("2016-01-19"), "John Doe"),
+     new Flight ( "DA40", new Date("2016-01-20"), new Date("2016-01-20"), "John Doe"),
+     new Flight ( "DA40", new Date("2016-03-18"), new Date("2016-03-18"), "John Doe"),
+     new Flight ( "DA40", new Date("2016-03-27"), new Date("2016-03-27"), "John Doe"),
+
    ]
     
 
